abad8f91d6f68c47842a7e7c57c09fcba1638801
Maciej Dyminski <maciej.dyminski02@gmail.com>
2021-09-23 11:16:25 +0200

    update site.js

--- Ksiegowosc.Intranet/wwwroot/js/site.js
+++ Ksiegowosc.Intranet/wwwroot/js/site.js
@@ -2,3 +2,45 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+
+$(document).ready(function () {
+    $('#view_details').on('click', function () {
+        $('#action_index').toggle();
+        $('#action').toggle();
+        $('#napis_kontrachent').toggle();
+    });
+    $('#filters').on('click', function () {
+        $('#filters_content').toggle();
+    });
+});
+
+$("#dropDown").change(function () {
+    $("#pageSize").submit();
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(function () {
+    $("table").resizableColumns();
+});
+
+$(function () {
+    var current = location.pathname;
+    $('.nav-tabs li a').each(function () {
+        var $this = $(this);
+        if (current.indexOf($this.attr('href')) !== -1) {
+            $this.addClass('active');
+        }
+    })
+});
+
+$(function () {
+    $(".table").on("click", "tr[role=\"button\"]", function (e) {
+        window.location = $(this).data("href");
+    });
+});
